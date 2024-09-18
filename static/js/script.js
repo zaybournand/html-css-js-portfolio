eb77c2c848ef4a263bbbdaf9e5c7a093fce5353b
zaybournand <zaybournand@gmail.com>
2024-09-18 12:12:50 -0400

    Update 2 script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,23 +1,8 @@
-// Function to toggle the menu
 function toggleMenu() {
     const hamburgerIcon = document.querySelector('.hamburger-icon');
     const menuLinks = document.querySelector('.menu-links');
-
+    
     // Toggle the 'open' class on the menu and hamburger icon
     menuLinks.classList.toggle('open');
     hamburgerIcon.classList.toggle('open');
 }
-
-// Add event listener to hamburger icon
-document.querySelector('.hamburger-icon').addEventListener('click', toggleMenu);
-
-// Optional: Close menu when clicking outside of it
-document.addEventListener('click', (event) => {
-    const menuLinks = document.querySelector('.menu-links');
-    const hamburgerIcon = document.querySelector('.hamburger-icon');
-
-    if (!hamburgerIcon.contains(event.target) && !menuLinks.contains(event.target)) {
-        menuLinks.classList.remove('open');
-        hamburgerIcon.classList.remove('open');
-    }
-});
